823f03d9e18818e2bb1683f3ace37981ffe4456c
J Mulder <jlmulder2014@gmail.com>
2020-09-17 17:15:13 -0700

    thought and user controller setup

---
+++ controllers/thought-controller.js
@@ -0,0 +1,28 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  getAllThoughts(req, res) {
+
+  },
+  getThoughtById({ params }, res) {
+
+  },
+  createThought({ body }, res) {
+
+  },
+  updateThought({ params, body }, res) {
+
+  },
+  deleteThought({ params }, res) {
+
+  },
+  addReaction() {
+
+  },
+  removeReaction() {
+
+  },
+
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,27 @@
+const { User } = require('../models');
+
+const userController = {
+  getAllUsers(req, res) {
+
+  },
+  getUserById({ params }, res) {
+
+  },
+  createUser({ body }, res) {
+
+  },
+  updateUser({ params, body }, res) {
+
+  },
+  deleteUser({ params }, res) {
+
+  },
+  addToFriendList() {
+    //?????
+  },
+  removefromFriendList() {
+    //?????
+  },
+};
+
+module.exports = userController;
