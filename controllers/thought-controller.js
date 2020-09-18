2c6f66d6d167b38c6f7c52357050c76577cf9ce1
J Mulder <jlmulder2014@gmail.com>
2020-09-17 18:29:09 -0700

    Controller methods

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,25 +2,102 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
   getAllThoughts(req, res) {
-
+    //mongoose method similar to sequelize .findall()
+    Thought.find({})
+      .populate({
+        //????
+        //dont care about __v field "-" means don't want. Otherwise ONLY
+        select: '-__v'
+      })
+      .select('-__v')
+      // sort by descending order by the _id value
+      .sort({ _id: -1 })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
   getThoughtById({ params }, res) {
-
+    Thought.findOne({ _id: params.id })
+      .populate({
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
   },
   addThought({ body }, res) {
-
+    console.log(body);
+    Thought.create(body)
+      //grab id, use to add comment
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          //use push method to add comment id, add data to array
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
   },
   updateThought({ params, body }, res) {
 
   },
   removeThought({ params }, res) {
-
+    Thought.findOneAndDelete({ _id: params.thoughtId })
+      .then(deletedThought => {
+        if (!deletedThought) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $pull: { comments: params.thoughtId } },
+          { new: true }
+        );
+      })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
   },
-  addReaction() {
-
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
   },
   removeReaction() {
-
+    Thought.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
   },
 
 };
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,19 +2,61 @@ const { User } = require('../models');
 
 const userController = {
   getAllUsers(req, res) {
-
+    //mongoose method similar to sequelize .findall()
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        //dont care about __v field "-" means don't want. Otherwise ONLY
+        select: '-__v'
+      })
+      .select('-__v')
+      // sort by descending order by the _id value
+      .sort({ _id: -1 })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
   getUserById({ params }, res) {
-
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
   },
   createUser({ body }, res) {
-
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
   },
   updateUser({ params, body }, res) {
-
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
   },
   deleteUser({ params }, res) {
-
+    User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
   },
   addToFriendList() {
     //?????
